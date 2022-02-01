2d124252e8f7a77a8fe64d8880569ee6f88d5300
BekembaevaSel <bekembaeva8@gmail.com>
2022-02-01 16:45:44 +0600

    home work 10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([])
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/photos?_limit=9')
+			.then((response) => response.json())
+			.then((data) => setData([...data]))
+	}, [])
+
+	return (
+		<div className='App'>
+			<div className='one-block'>
+				{data.map((element) => (
+					<div className='as' 
+					key={element.id}>
+						{element.title}
+						<div>
+							<img className='photos' src={element.url} />
+							{element.thumbnailUrl}
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
